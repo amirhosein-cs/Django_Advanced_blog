e9cd72de73e791451ada2c898d91605835e62ea7
amir <amir.heydari44445@gmail.com>
2026-09-08 12:14:04 +0330

    creat models and admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Category
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','status','author','created_date')
+    list_filter = ('status',)
+    search_fields = ("author",)
+    ordering = ("-created_date",)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+admin.site.register(Post)
+admin.site.register(Category)
--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.forms import Media
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,9 +118,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = 'accounts.User'
